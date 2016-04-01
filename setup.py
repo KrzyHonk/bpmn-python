633695ac70e94f840c1db820c85c604393732993
KrzyHonk <honkiszkrzysztof@gmail.com>
2016-04-01 21:35:11 +0200

    Added basic package information to setup.py. IMPORTANT - added NetworkX dependency to setup.py.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "bpmn_python",
+    version = "0.0.1",
+    author = "Izbela Smietana, Krzysztof Honkisz",
+    #author_email = "honkiszkrzystof@gmail.com",
+    description = ("Python library that allows to import/export BPMN diagram (as an XML file) and provides a simple visualization capabilities."),
+    license = "GNU GENERAL PUBLIC LICENSE",
+    keywords = "bpmn",
+    #url = "",
+    packages=['bpmn_python'],
+    install_requires=[
+          'networkx',
+    ],
+    long_description=read('README.md'),
+)
+
