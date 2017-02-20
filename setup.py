6469a7e5d61c7f6296ac7e6e5a22d6a3e68dc812
KrzyHonk <honkiszkrzysztof@gmail.com>
2017-02-20 21:07:41 +0100

    - Updates setup.py file for version 0.0.3

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
+# coding=utf-8
 import os
+
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
+
 def read(fname):
+    """
+    Utility function to read the README file. Used for the long_description.
+    :param fname:
+    :return:
+    """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "bpmn_python",
-    version = "0.0.2",
-    author = "Izbela Smietana, Krzysztof Honkisz",
-    #author_email = "honkiszkrzystof@gmail.com",
-    description = ("Python library that allows to import/export BPMN diagram (as an XML file) and provides a simple visualization capabilities."),
-    license = "GNU GENERAL PUBLIC LICENSE",
-    keywords = ["bpmn", "xml"],
-    url = "https://github.com/KrzyHonk/bpmn-python",
-    download_url="https://github.com/KrzyHonk/bpmn-python/tarball/0.0.2",
+    name="bpmn_python",
+    version="0.0.3",
+    author="Izbela Smietana, Krzysztof Honkisz",
+    # author_email = "honkiszkrzystof@gmail.com",
+    description=("Python library that allows to import/export BPMN diagram (as an XML file) and provides a simple "
+                 "visualization capabilities."),
+    license="GNU GENERAL PUBLIC LICENSE",
+    keywords=["bpmn", "xml"],
+    url="https://github.com/KrzyHonk/bpmn-python",
+    download_url="https://github.com/KrzyHonk/bpmn-python/tarball/0.0.3",
     packages=['bpmn_python'],
     install_requires=[
         'networkx',
@@ -26,4 +32,3 @@ setup(
     ],
     long_description=read('README.md'),
 )
-
