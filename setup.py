1165c5fb562923ee9e971363606f112a3312ee27
KrzyHonk <honkiszkrzysztof@gmail.com>
2017-03-28 21:03:59 +0200

    Fixing prbolem with missing packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(fname):
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="bpmn_python",
-    version="0.0.5",
+    version="0.0.7",
     author="Izbela Smietana, Krzysztof Honkisz",
     # author_email = "honkiszkrzystof@gmail.com",
     description=("Python library that allows to import/export BPMN diagram (as an XML file) and provides a simple "
@@ -23,8 +23,8 @@ setup(
     license="GNU GENERAL PUBLIC LICENSE",
     keywords=["bpmn", "xml"],
     url="https://github.com/KrzyHonk/bpmn-python",
-    download_url="https://github.com/KrzyHonk/bpmn-python/tarball/0.0.5",
-    packages=['bpmn_python'],
+    download_url="https://github.com/KrzyHonk/bpmn-python/tarball/0.0.7",
+    packages=find_packages(exclude=['docs', 'tests*']),
     install_requires=[
         'networkx',
         'matplotlib',
