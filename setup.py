ab21b726bd17c5c8e6f70c1d05b9024e62d49372
KrzyHonk <honkiszkrzysztof@gmail.com>
2017-07-13 15:31:41 +0200

    Version 0.0.16 release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="bpmn_python",
-    version="0.0.16-SNAPSHOT",
+    version="0.0.16",
     author="Izbela Smietana, Krzysztof Honkisz",
     # author_email = "honkiszkrzystof@gmail.com",
     description=("Python library that allows to import/export BPMN diagram (as an XML file) and provides a simple "
@@ -23,7 +23,7 @@ setup(
     license="GNU GENERAL PUBLIC LICENSE",
     keywords=["bpmn", "xml"],
     url="https://github.com/KrzyHonk/bpmn-python",
-    download_url="https://github.com/KrzyHonk/bpmn-python/tarball/0.0.16-SNAPSHOT",
+    download_url="https://github.com/KrzyHonk/bpmn-python/tarball/0.0.16",
     packages=find_packages(exclude=['docs', 'tests*']),
     install_requires=[
         'networkx',
