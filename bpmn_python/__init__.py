0405d9d4bed3cd2d054bee1ad02d9bd2818beee8
Patryk Galczynski <galczynski.patryk@gmail.com>
2019-01-05 15:58:22 +0100

    fix doc strings in multiple py files

--- bpmn_python/__init__.py
+++ bpmn_python/__init__.py
@@ -2,6 +2,6 @@
 """
 Package init file
 """
-__all__ = ["bpmn_diagram_exception", "bpmn_diagram_export", "bpmn_diagram_import", "bpmn_diagram_layouter",
+__all__ = ["bpmn_diagram_export", "bpmn_diagram_import", "bpmn_diagram_layouter",
            "bpmn_diagram_exception", "bpmn_diagram_metrics", "bpmn_diagram_visualizer", "bpmn_import_utils",
            "bpmn_process_csv_export", "diagram_layout_metrics", "grid_cell_class", "bpmn_diagram_rep"]
